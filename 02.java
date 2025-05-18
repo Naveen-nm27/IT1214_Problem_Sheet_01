86c650b235f9e72e89a6b1c45ff751b3284f7f0b
Naveen.mn <naveenm2795@gmail.com>
2025-05-18 09:02:09 +0000

    problem02

--- 02.java
+++ 02.java
@@ -2,19 +2,18 @@ class Vehicle{
     private String licencePlate;
     private String ownerName;
     private int hourseParked;
-    private int parkLimit = 5;
 
-    Vehicle(String lp,String on,int hp,int pl){
+    Vehicle(String lp,String on,int hp){
         licencePlate = lp;
         ownerName = on;
         hourseParked = hp;
     }
 
-    public String getlicencePlate(){
+    public String getLicencePlate(){
         return licencePlate;
     }
 
-    public String getownerName(){
+    public String getOwnerName(){
         return ownerName;
     }
 
@@ -22,22 +21,51 @@ class Vehicle{
         return hourseParked;
     }
 
-    public void setlicencePlate(String licencePlate){
+    public void setLicencePlate(String licencePlate){
         this.licencePlate = licencePlate;
     }
 
-    public void setownerName(String ownerName){
+    public void setOwnerName(String ownerName){
         this.ownerName = ownerName;
     }
 
-    public void sethourseParked(String hourseParked){
+    public void setHourseParked(int hourseParked){
         this.hourseParked = hourseParked;
     }
 
 }
 
 class ParkingLot{
-    String parkedCars[] = new String[parkLimit];
+    private int parkLimit = 0;
+    String parkedCarsLp[] = new String[4];
+    String parkedOwner[] = new String[4];
+    int parkedHours[] = new int[4];
 
-    
+    public void addVehicles(String lp,String owner,int hours){
+         if(parkLimit < 4){
+            parkedCarsLp[parkLimit] = lp;
+            parkedOwner[parkLimit] = owner;
+            parkedHours[parkLimit] = hours;
+            parkLimit++;
+        }
+    }
+
+    public void rmVehicles(String licencePlate){
+        if(licencePlate)
+    }
+
+}
+
+class problem02 {
+    public static void main(String[] args){
+        Vehicle v01 = new Vehicle("ABC123","John Doe",2);
+        Vehicle v02 = new Vehicle("XYZ789","Jane Smith",4);
+        Vehicle v03 = new Vehicle("LMN456","Bob Brown",1);
+
+        ParkingLot p01 = new ParkingLot();
+
+        p01.addVehicles(v01.getLicencePlate(),v02.getOwnerName(),v03.gethourseParked());
+    }
+
+
 }
