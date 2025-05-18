3aae39e4316ead4cff0b0b857f4ba3d79161ee5c
Naveen.mn <naveenm2795@gmail.com>
2025-05-18 01:58:58 +0000

    01

---
+++ 01.java
@@ -0,0 +1,49 @@
+class Student{
+    private int studentId;
+    private String name;
+    private int daysAttended;
+
+Student(int studentId,String name,int daysAttended){
+    this.studentId = studentId;
+    this.name = name;
+    this.daysAttended = daysAttended;
+
+    public String getStudentId(){
+        return studentId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getdaysAttended(){
+        return daysAttended;
+    }
+
+    public void setStudentId(int S1){
+        studentId = S1;
+    }
+
+    public void setName(String S1){
+        name = S1;
+    }
+
+    public void setdatyAttended(int days){
+        daysAttended = days;
+    }
+
+}
+
+class Classroom{
+    int classStudent[] = new classStudent[10];
+
+    
+}
+
+}
+
+class 01{
+    public static void main(String[] args){
+
+    }
+}
