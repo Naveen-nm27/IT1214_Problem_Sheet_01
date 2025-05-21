067072cda0b76b8b235e6963e79fa23f2406b715
Naveen.mn <naveenm2795@gmail.com>
2025-05-21 23:42:20 +0530

    p05 Done

--- 05.java
+++ 05.java
@@ -10,26 +10,22 @@ class Students{
 		if(marks<=100 && marks>=0){
 			return true;
 			}
-		else{
-			return false;
-			throw new Exception("Erroer");
-		}
+		return false;
 	}
 	
-	Students(String name,int exam1,int exam2,int exam3) throws Exception{
-			if(validateMarks(exam1) && validateMarks(exam2) && validateMarks(exam3)){
-				this.name = name;
-				this.exam1 = exam1;
-				this.exam2 = exam2;
-				this.exam3 = exam3;
-			} else{
-				System.out.println("Settings the marks values to 0");
-				throw new Exception("Exam scores must be between 0 and 100.");
-			}				
-	}
+  public Students(String name, int exam1, int exam2, int exam3) throws Exception {
+        if (validateMarks(exam1) && validateMarks(exam2) && validateMarks(exam3)) {
+            this.name = name;
+            this.exam1 = exam1;
+            this.exam2 = exam2;
+            this.exam3 = exam3;
+        } else {
+            throw new Exception("Exam scores must be between 0 and 100.");
+        }
+    }
 	
 	public static void setScores(int e1,int e2,int e3){
-		
+
 	}
 	
 	public String getName(){
@@ -57,34 +53,19 @@ class Students{
 		
 	}
 
-
 }
 
-class LabSheet{
+class Problem05{
 	public static void main(String[] args){
-		
-		try{
-			Students St = new Students("naveen",75,110,-40);
-			
-			
-			System.out.println("Student Name is: " + St.getName());
-			System.out.println("Score For Exam 1 is: " + St.getScoreE1());
-			System.out.println("Score For Exam 2 is: " + St.getScoreE2());
-			System.out.println("Score For Exam 3 is: " + St.getScoreE3());
-			System.out.println("Student Average is: " + St.getAvarage());
-			
-			Students St2 = new Students("jhon",99,88,75);
-			
-			System.out.println("Student Name is: " + St2.getName());
-			System.out.println("Score For Exam 1 is: " + St2.getScoreE1());
-			System.out.println("Score For Exam 2 is: " + St2.getScoreE2());
-			System.out.println("Score For Exam 3 is: " + St2.getScoreE3());
-			System.out.println("Student Average is: " + St2.getAvarage());
-		} catch (Exception e){
-
-			System.out.println("Exception is: " + e.getMessage());
-		}
-
-
-	}
+    try {
+            Students st01 = new Students("John", 75, 110, 90);
+            System.out.println("Name: " + st01.getName());
+            System.out.println("Exam 1: " + st01.getScoreE1());
+            System.out.println("Exam 2: " + st01.getScoreE2());
+            System.out.println("Exam 3: " + st01.getScoreE3());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("Student data not displayed due to validation error.");
+        }
+}
 }
