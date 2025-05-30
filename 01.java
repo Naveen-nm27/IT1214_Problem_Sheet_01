cfe736976a388bf80621f274aab1d5d0c6bf5b3b
Naveen.mn <naveenm2795@gmail.com>
2025-05-30 06:56:57 +0530

    problem01 Multiple array issue fixed and used a single array of Student class to store data

--- 01.java
+++ 01.java
@@ -1,134 +1,108 @@
-class Student{
+class Student {
     private int studentId;
     private String name;
     private int daysAttended;
-
-Student(int studentId,String name,int daysAttended){
-    this.studentId = studentId;
-    this.name = name;
-    this.daysAttended = daysAttended;
-}
-    public int getStudentId(){
+    
+    Student(int studentId, String name, int daysAttended) {
+        this.studentId = studentId;
+        this.name = name;
+        this.daysAttended = daysAttended;
+    }
+    
+    public int getStudentId() {
         return studentId;
     }
-
-    public String getName(){
+    
+    public String getName() {
         return name;
     }
-
-    public int getdaysAttended(){
+    
+    public int getDaysAttended() {
         return daysAttended;
     }
-
-    public void setStudentId(int S1){
-        studentId = S1;
+    
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
     }
-
-    public void setName(String S1){
-        name = S1;
+    
+    public void setName(String name) {
+        this.name = name;
     }
-
-    public void setdatyAttended(int days){
-        daysAttended = days;
+    
+    public void setDaysAttended(int days) {
+        this.daysAttended = days;
     }
-
 }
 
-class Classroom{
-    String stName[] = new String[10];
-    int stid[] = new int[10];
-    int daysAttended[] = new int[10]; 
+class Classroom {
+    Student[] students = new Student[10];
     int numberOfStudents = 0;
     
-
-    public void addStudent(String S1,int id,int date){
-        if(numberOfStudents <= 9){
-            stName[numberOfStudents] = S1;
-            stid[numberOfStudents] = id;
-            daysAttended[numberOfStudents] = date;
+    public void addStudent(String name, int id, int daysAttended) {
+        if (numberOfStudents <= 9) {
+            students[numberOfStudents] = new Student(id, name, daysAttended);
             numberOfStudents++;
-        }else{
-            System.out.println("Maximum Naumber of Studnts are added");
+        } else {
+            System.out.println("Maximum Number of Students are added");
         }
-
     }
-
-    public boolean validateID(int id){
-            for(int item:stid){
-                if(item == id){
-                    return true;
-                }
+    
+    public boolean validateID(int id) {
+        for (Student student : students) {
+            if (student != null && student.getStudentId() == id) {
+                return true;
             }
-            return false;
         }
-
-
-    public void stDetails(int id){
-
-        if(validateID(id)){
-            for(int i = 0; i <= numberOfStudents;i++){
-                if(id == stid[i]){
-                    System.out.println("Student Id - " + stid[i]);
+        return false;
+    }
+    
+    public void stDetails(int id) {
+        if (validateID(id)) {
+            for (int i = 0; i < numberOfStudents; i++) {
+                if (id == students[i].getStudentId()) {
+                    System.out.println("Student Id - " + students[i].getStudentId());
                     System.out.println("+----------------+");
-                    System.out.println("Student Name: " + stName[i]);
-                    System.out.println("Attendence: "+daysAttended[i]+" Days");
+                    System.out.println("Student Name: " + students[i].getName());
+                    System.out.println("Attendance: " + students[i].getDaysAttended() + " Days");
                     System.out.println("+----------------+");
                     System.out.println();
-                } 
+                }
             }
         } else {
             System.out.println("Student is not Enrolled in this class");
         }
-
     }
-
-    public void editAttendance(int id,int newDate){
-        if (validateID(id)){
-                    for(int i = 0;i <= numberOfStudents;i++){
-            if(id == stid[i]){
-                daysAttended[i] = newDate;
-            }
-        }
-        } else{
-            System.out.println("Student " +id+ " is not enrolled in this class");
-        }
-
-    }
-
-    public void printClass(){
-        System.out.println(("Student ID | \tStudent Name | \tAttendance"));
-            for(int i = 0;i<=numberOfStudents;i++){
-            if(stid[i] != 0){
-            System.out.println("-" + stid[i] + "\t\t" + stName[i] + "\t" + daysAttended[i]);
-            }
-        }
-    }
-
     
+    public void editAttendance(int id, int newDays) {
+        if (validateID(id)) {
+            for (int i = 0; i < numberOfStudents; i++) {
+                if (id == students[i].getStudentId()) {
+                    students[i].setDaysAttended(newDays);
+                }
+            }
+        } else {
+            System.out.println("Student " + id + " is not enrolled in this class");
+        }
+    }
+    
+    public void printClass() {
+        System.out.println("Student ID | \tStudent Name | \tAttendance");
+        for (int i = 0; i < numberOfStudents; i++) {
+            System.out.println("-" + students[i].getStudentId() + "\t\t" + students[i].getName() + "\t" + students[i].getDaysAttended());
+        }
+    }
 }
 
-
-class Problem01{
-    public static void main(String[] args){
-        Student st01 = new Student(101,"Alice Smith",12);
-        Student st02 = new Student(102,"Bob Jones",15);
-        Student st03 = new Student(103,"Carol Lee",10);
-        
+class Problem01 {
+    public static void main(String[] args) {
         Classroom class01 = new Classroom();
-
-        class01.addStudent(st01.getName(),st01.getStudentId(),st01.getdaysAttended());
-        class01.addStudent(st02.getName(),st02.getStudentId(),st02.getdaysAttended());
-        class01.addStudent(st03.getName(),st03.getStudentId(),st03.getdaysAttended());
-
-        class01.editAttendance(104,16);
-
-        //class01.stDetails(101);
-        //class01.stDetails(102);
-        //class01.stDetails(103);
-        //class01.stDetails(104);
-
+        
+        // Add students directly in one line each
+        class01.addStudent("Alice Smith", 101, 12);
+        class01.addStudent("Bob Jones", 102, 15);
+        class01.addStudent("Carol Lee", 103, 10);
+        
+        class01.editAttendance(102, 16);
         class01.printClass();
-
-
     }
 }
